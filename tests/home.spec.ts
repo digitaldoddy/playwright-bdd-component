9e78706065a35fa20de12326748451146825b938
Daniel Dodson <danieldodson10@gmail.com>
2025-07-16 19:12:46 -0400

    first commit

---
+++ tests/home.spec.ts
@@ -0,0 +1,29 @@
+import { test as testBase, expect } from '@playwright/test';
+import { http } from 'msw';
+import { createNetworkFixture, type NetworkFixture } from '@msw/playwright'
+
+interface Fixtures {
+  network: NetworkFixture
+}
+
+const test = testBase.extend<Fixtures>({
+  network: createNetworkFixture({
+    initialHandlers: [
+      http.get('/resource', () => {
+        return new Response('hello world')
+      }),
+    ],
+  }),
+})
+
+testBase('homepage has greeting', async ({ page }) => {
+  await page.goto('/');
+  const data = await page.evaluate(() => {
+    return fetch('/resource').then((response) => {
+      return response.text()
+    })
+  })
+
+  expect(data).toBe('hello world')
+  await expect(page.getByText('Hello from React!')).toBeVisible();
+});
